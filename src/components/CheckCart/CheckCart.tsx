4b5aa815a43ececb478fa1428caecfed35d1fffe
Varga Albert <albi1000@icloud.com>
2023-11-11 12:05:29 +0100

    refactoring scss module CheckCart

--- src/components/CheckCart/CheckCart.tsx
+++ src/components/CheckCart/CheckCart.tsx
@@ -1,10 +1,9 @@
 import { Link } from "react-router-dom";
 import { useCardContext } from "../../context/CardContext";
 
-import "./CheckCart.scss"
 import CheckShopCartItems from "./CheckShopCartItems/CheckShopCartItems";
 
-
+import Style from "./CheckCart.module.scss"
 
 const CheckCart = () => {
 
@@ -17,36 +16,37 @@ const CheckCart = () => {
 
     const cartItemsContentTSX =
         cartItems.length === 0 ? (
-            <div className="check-car-zero">Még nincsenek termékek a kosaradban!</div>
+            <div className={Style.carZero}>Még nincsenek termékek a kosaradban!</div>
         ) : (
             cartItems.map((item) => <CheckShopCartItems key={item.id} {...item} />).reverse()
         );
 
     return (
-        <div className="check-cart-main-container">
-            <div className="check-cart-container">
-                <h2 className="cart-title">KOSARAM</h2>
+
+        <div className={Style.mainContainer}>
+            <div className={Style.cartContainer}>
+                <h2 className={Style.cartTitle}>KOSARAM</h2>
                 {cartItemsContentTSX}
             </div>
-            <div className="check-sum-container" >
-                <div className="check-sum">
+            <div className={Style.sumContainer} >
+                <div className={Style.checkSum}>
                     <h3>Összegzés:</h3>
-                    <div className="check-sum-content">
-                        <div className="text">
-                            <div className="format-font">Kosár részösszeg:</div><div className="format-font">{cardSum(false)} Ft</div>
+                    <div className={Style.sumContent}>
+                        <div className={Style.text}>
+                            <div className={Style.formatFont}>Kosár részösszeg:</div><div className="format-font">{cardSum(false)} Ft</div>
                         </div>
-                        <div className="text">
-                            <div className="format-font">Szállítási díj:</div><div className="format-font">{formatPrice(shippingFee)} Ft</div>
+                        <div className={Style.text}>
+                            <div className={Style.formatFont}>Szállítási díj:</div><div className="format-font">{formatPrice(shippingFee)} Ft</div>
                         </div>
-                        <div className="text">
-                            <div className="format-font">Kedvezmény:</div><div className="format-font">0 Ft</div>
+                        <div className={Style.text}>
+                            <div className={Style.formatFont}>Kedvezmény:</div><div className="format-font">0 Ft</div>
                         </div>
-                        <div className="text">
-                            <div className="format-font">Összesen:</div><div className="format-font">{cardSum(true)} Ft</div>
+                        <div className={Style.text}>
+                            <div className={Style.formatFont}>Összesen:</div><div className="format-font">{cardSum(true)} Ft</div>
                         </div>
                     </div>
                     <button>TOVÁBB A KASSZÁHOZ</button>
-                    <Link className="check-sum-link" to="/" >
+                    <Link className={Style.link} to="/" >
                         Tovább vásárolok
                     </Link>
                 </div>
