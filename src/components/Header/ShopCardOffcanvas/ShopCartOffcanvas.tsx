7873b7195a99cd6f141842080c71fd633ea516f6
VargaAlbert <albi1000@icloud.com>
2023-12-11 17:33:38 +0100

    ShopCartOffcanvas css module refactoring

--- src/components/Header/ShopCardOffcanvas/ShopCartOffcanvas.tsx
+++ src/components/Header/ShopCardOffcanvas/ShopCartOffcanvas.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import ShopCartItems from "./ShopCartItems/ShopCartItems";
 
-import "./ShopCartOffcanvas.scss"
+import Style from "./ShopCartOffcanvas.module.scss"
 
 const ShopCartOffcanvas: React.FC = () => {
 
@@ -18,30 +18,30 @@ const ShopCartOffcanvas: React.FC = () => {
 
     const cartItemsContentTSX =
         cartItems.length === 0 ? (
-            <div className="car-zero">Még nincsenek termékek a kosaradban!</div>
+            <div className={Style.carZero}>Még nincsenek termékek a kosaradban!</div>
         ) : (
             cartItems.map((item) => <ShopCartItems key={item.id} {...item} />).reverse()
         );
 
     return (
-        <div className="card-offcanvas-container">
-            <Offcanvas show={show} onHide={handleClose} placement="end">
+        <div>
+            <Offcanvas className={Style.backgroung} show={show} onHide={handleClose} placement="end">
                 <Offcanvas.Header closeButton>
-                    <div className="offcanvas-icon-container">
+                    <div className={Style.offcanvasIconContainer}>
                         <FaCartShopping className="icon" />
                         <Offcanvas.Title>KOSARAM</Offcanvas.Title>
                     </div>
                 </Offcanvas.Header>
                 <Offcanvas.Body className="position-relative p-0">
                     {cartItemsContentTSX}
-                    <div className="car-sum-cont">
+                    <div className={Style.carSumCont}>
                         <div>
-                            <p className="car-sum-text">ÖSSZESEN:</p>
-                            <p className="car-sum-price">
+                            <p className={Style.carSumText}>ÖSSZESEN:</p>
+                            <p className={Style.carSumPrice}>
                                 {cardSum(false)} Ft
                             </p>
                         </div>
-                        <Link className="cart-link" to="/check-cart">
+                        <Link className={Style.cartIink} to="/check-cart">
                             <button>TOVÁBB A PÉNZTÁRHOZ</button>
                         </Link>
                     </div>
