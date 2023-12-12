881af9bf07107724272561b7d389990a4793c87e
VargaAlbert <albi1000@icloud.com>
2023-12-12 14:36:34 +0100

    HomePage css module refactoring

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import "./HomePage.scss"
 import ProductCarousel from './ProductCarousel/ProductCarousel';
 import HomeCarousel from './HomeCarousel/HomeCarousel';
 
+import "./HomePage.scss"
+import Style from "./HomePage.module.scss"
+
 const HomePage: React.FC = () => {
     return (
-        <div className="HomePage-main-container">
+        <div className={Style.mainContainer}>
             <HomeCarousel />
             <ProductCarousel />
         </div>
