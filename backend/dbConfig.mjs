691b2027577867b079cb572519c4d2993fbd4d2f
VargaAlbert <albi1000@icloud.com>
2023-12-13 11:36:57 +0100

    full version 2

---
+++ backend/dbConfig.mjs
@@ -0,0 +1,8 @@
+const dbConfig = {
+    mongoURI: "mongodb+srv://albi:Ais6A45SJpDP3n5r@fishing-ecommerce.n8amumh.mongodb.net/?retryWrites=true&w=majority",
+};
+
+export default dbConfig;
+
+
+//mongodb+srv://albi:Ais6A45SJpDP3n5r@fishing-ecommerce.n8amumh.mongodb.net/?retryWrites=true&w=majority
