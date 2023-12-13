d9679f2c1d1fee0d9f8ffc58fc7077cf8b941e98
Varga Albert <albi1000@icloud.com>
2023-12-13 11:43:27 +0100

    Delete backend/dbConfig.mjs

--- backend/dbConfig.mjs
+++
@@ -1,8 +0,0 @@
-const dbConfig = {
-    mongoURI: "mongodb+srv://albi:Ais6A45SJpDP3n5r@fishing-ecommerce.n8amumh.mongodb.net/?retryWrites=true&w=majority",
-};
-
-export default dbConfig;
-
-
-//mongodb+srv://albi:Ais6A45SJpDP3n5r@fishing-ecommerce.n8amumh.mongodb.net/?retryWrites=true&w=majority
